c0eab3351c180ffc9a182953b064ed7c7ca0169c
roberdiles <61599325+roberdiles@users.noreply.github.com>
2024-03-31 21:18:09 -0300

    Se implementa botón con cambios de estado

--- SmartGatito.ino
+++ SmartGatito.ino
@@ -1,51 +1,71 @@
-//#include <Adafruit_LiquidCrystal.h>
+#include <Adafruit_LiquidCrystal.h>
 
-//Adafruit_LiquidCrystal lcd_1(0);
+Adafruit_LiquidCrystal lcd_1(0);
 const int pingPin = A0; // Trigger Pin of Ultrasonic Sensor
 const int echoPin = A1; // Echo Pin of Ultrasonic Sensor
 const int pinOut = 6;
 const double r2 = 0.2809; //Radio del estanque en metros al cuadrado
 const double pi = 3.1415926535897932384626433832795;
 double v = 0;
+int modo = 1; // Variable para almacenar el modo actual
+
 void setup() {
    Serial.begin(9600); // Starting Serial Terminal
-   //lcd_1.begin(16, 2);
-   //lcd_1.setBacklight(1);
+   lcd_1.begin(16, 2);
+   lcd_1.setBacklight(1);
    pinMode(6, OUTPUT);
-   
+   pinMode(2, INPUT_PULLUP); // Botón conectado al pin 2
 }
 
 void loop() {
-   //lcd_1.setCursor(0, 0);
-   long duration, inches;
-   double cm;
-   pinMode(pingPin, OUTPUT);
-   digitalWrite(pingPin, LOW);
-   delayMicroseconds(2);
-   digitalWrite(pingPin, HIGH);
-   delayMicroseconds(10);
-   digitalWrite(pingPin, LOW);
-   pinMode(echoPin, INPUT);
-   duration = pulseIn(echoPin, HIGH);
-   cm = microsecondsToCentimeters(duration);
-   //v = 1000-h*pi*r2*1000;
-   // lcd_1.print(v);
-   //lcd_1.print("L               ");
+  // Lectura del estado del botón
+  int estadoBoton = digitalRead(2);
 
-  if(cm>30){
-    //lcd_1.setCursor(0, 1);
-    //lcd_1.print("Gatito Cerca         ");
-    //Apagar Bomba
-     digitalWrite(pinOut, HIGH);
-  }else{
-    //lcd_1.setCursor(0, 1);
-    //lcd_1.print("Gatito Lejo         ");
-    //Prender Bomba
-     digitalWrite(pinOut, LOW);
+  // Cambiar el modo al presionar el botón
+  if (estadoBoton == LOW) {
+    modo++;
+    if (modo > 3) modo = 1; // Se reinicia el ciclo
+    Serial.println("Nuevo modo:");
+    Serial.println(modo);
+    delay(500);
   }
-    delay(100);
 
-  
+  // Ejecutar el modo correspondiente
+  switch (modo) {
+    case 1:
+      // Modo siempre encendido
+      digitalWrite(pinOut, LOW); // Encender el LED
+      break;
+    case 2:
+      // Modo dependiendo de la medición del ultrasonido
+      long duration, inches;
+      double cm;
+      lcd_1.setCursor(0, 0);
+      pinMode(pingPin, OUTPUT);
+      digitalWrite(pingPin, LOW);
+      delayMicroseconds(2);
+      digitalWrite(pingPin, HIGH);
+      delayMicroseconds(10);
+      digitalWrite(pingPin, LOW);
+      pinMode(echoPin, INPUT);
+      duration = pulseIn(echoPin, HIGH);
+      cm = microsecondsToCentimeters(duration);
+      if (cm < 30) {
+        lcd_1.setCursor(0, 1);
+        lcd_1.print("Gatito Cerca         ");
+        digitalWrite(pinOut, HIGH); // Apagar Bomba
+      } else {
+        lcd_1.setCursor(0, 1);
+        lcd_1.print("Gatito Lejos         ");
+        digitalWrite(pinOut, LOW); // Prender Bomba
+      }
+      delay(100);
+      break;
+    case 3:
+      // Modo LED apagado
+      digitalWrite(pinOut, HIGH); // Apagar el LED
+      break;
+  }
 }
 
 long microsecondsToInches(long microseconds) {
