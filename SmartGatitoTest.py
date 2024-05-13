1a616a3a53ecd073e9dcb641d154cf62d0c7163b
Felipe Sanzana <pipesanzana12@gmail.com>
2024-05-13 10:35:02 -0400

    mqtt

--- SmartGatitoTest.py
+++ SmartGatitoTest.py
@@ -1,6 +1,7 @@
 import RPi.GPIO as GPIO
 import time
 import requests
+import paho.mqtt.client as mqtt
 
 # Definición de pines
 trigPin = 24  # Pin trigger del sensor ultrasónico (GPIO18)
@@ -9,8 +10,13 @@ pinRele = 14   # Pin para controlar el relé (GPIO8)
 
 # Variables
 modo = 2  # Modo inicial
-contador_agua = 0  # Contador de veces que el gato bebe agua
-
+agua = 0  # Contador de veces que el gato bebe agua
+# Configuración MQTT
+broker_address = "mqtt.thingsboard.cloud"
+port = 1883 
+client = mqtt.Client("30vte5orp3pywd6get3n")  # Crear nuevo objeto de instancia
+client.username_pw_set("emzn7leosagnh376l84e", "ouokpwl7dyfo0xdv1pwn")  # Configurar usuario y contraseña
+client.connect(broker_address, port)  # Conectar al broker
 def setup():
     GPIO.setmode(GPIO.BCM)  # Configuración de numeración de pines
     GPIO.setup(pinRele, GPIO.OUT)  # Pin del relé como salida
@@ -18,6 +24,8 @@ def setup():
     GPIO.setup(echoPin, GPIO.IN)  # Pin del sensor de ultrasonido como entrada
     GPIO.output(trigPin, False)  # Inicializar el pin del sensor de ultrasonido en bajo
     time.sleep(2)  # Esperar 2 segundos para estabilizar el sensor
+    
+    
 
 def loop():
     global modo
@@ -43,32 +51,25 @@ def loop():
         if cm < 30:
             print("Gatito Cerca")
             GPIO.output(pinRele, GPIO.LOW)  # Encender la bomba
-            contador_agua += 1  # Incrementar el contador de veces que el gato bebe agua
+            agua = 1  # Incrementar el contador de veces que el gato bebe agua
+            send_telemetry(agua)
             time.sleep(3)  # Tiempo que la bomba estará encendida (8 segundos
         else:
             print("Gatito Lejos")
             GPIO.output(pinRele, GPIO.HIGH)  # Apagar la bomba
+            agua = 0  # Reiniciar el contador de veces que el gato bebe agua
+            send_telemetry(agua)
         time.sleep(0.1)  # Retardo para estabilidad
     elif modo == 3:
         # Modo siempre apagado
         GPIO.output(pinRele, GPIO.HIGH)  # Apagar la bomba
         print("Fuente Apagada")
+    time.sleep(1)  # Retardo para estabilidad
 
-    # Enviar mensaje de telemetría a ThingsBoard
-    send_telemetry()
-
-def send_telemetry():
-    global contador_agua
+def send_telemetry(agua):
     # Enviar el contador de veces que el gato bebe agua a ThingsBoard
-    url = 'http://thingsboard.cloud/api/v1/7mTCa4pj3NWSnJFZ9hlo/telemetry'
-    payload = {'contador_agua': contador_agua}
-    headers = {'Content-Type': 'application/json', 'Accept': 'application/json'}
     try:
-        response = requests.post(url, json=payload, headers=headers)
-        if response.status_code == 200:
-            print("Contador de agua enviado a ThingsBoard:", contador_agua)
-        else:
-            print("Error al enviar el contador de agua a ThingsBoard:", response.text)
+        client.publish("v1/devices/me/telemetry", "{agua:"+str(agua)+"}")
     except Exception as e:
         print("Error de conexión:", e)
 
