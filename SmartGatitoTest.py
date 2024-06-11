7f47bca3e2a410705766463ccaca7e2211c74729
Felipe Sanzana <pipesanzana12@gmail.com>
2024-06-10 20:07:36 -0400

    Added Cat face detection system

--- SmartGatitoTest.py
+++ SmartGatitoTest.py
@@ -4,6 +4,8 @@ import paho.mqtt.client as mqtt
 import threading
 import json
 import requests
+import cv2 
+from picamera2 import Picamera2
 
 
 with open('secrets.json') as file:
@@ -34,7 +36,10 @@ cm = 0  # Distancia en centímetros
 modo = 2  # Modo inicial
 agua = 0  # Contador de veces que el gato bebe agua
 token = ""
-
+face_cascade = cv2.CascadeClassifier('./CatRecognitionSystem/haarcascade_frontalcatface.xml')  
+cap = Picamera2()
+isGatito = False
+detectado = False
 # Configuración MQTT
 
 client = mqtt.Client(clientId)  # Crear nuevo objeto de instancia
@@ -56,6 +61,7 @@ time.sleep(2)  # Esperar 2 segundos para estabilizar el sensor
     
 def distanceMonitor(): # Función para medir distancia
     global cm
+    global detectado
     try:
         while True:
             #print("midiendo distancia")
@@ -69,7 +75,11 @@ def distanceMonitor(): # Función para medir distancia
             duration = pulse_end - pulse_start
             cm = duration * 17150
             cm =  round(cm, 2)
-            #print("Distancia:", cm, "cm")   
+            if cm < 30:
+                detectado = True
+                send_telemetry("detectado")
+            else:
+                detectado = False
             time.sleep(1)
     except KeyboardInterrupt:
         GPIO.cleanup()
@@ -77,7 +87,8 @@ def distanceMonitor(): # Función para medir distancia
 
 def modeSwitch(): # Función para cambiar modo de la fuente
     global modo
-    global cm
+    global isGatito
+    global detectado
     try:
         while True:
             print("Modo:", modo)
@@ -85,9 +96,9 @@ def modeSwitch(): # Función para cambiar modo de la fuente
                 GPIO.output(pinRele, GPIO.LOW)  # Encender la bomba
                 print("Fuente Encendida")
             elif modo == 2:
-                if cm < 30:
+                if detectado and isGatito:
                     GPIO.output(pinRele, GPIO.LOW)  # Encender la bomba
-                    send_telemetry()
+                    send_telemetry("agua")
                     time.sleep(2)
                 else:
                     GPIO.output(pinRele, GPIO.HIGH)  # Apagar la bomba    
@@ -103,7 +114,7 @@ def subscriber(): # Función para suscribirse al topic
     client.loop_forever()
     
 
-def send_telemetry(): # Función para enviar señal a ThingsBoard cada vez que el gato bebe agua
+def send_telemetry(key): # Función para enviar señal a ThingsBoard cada vez que el gato bebe agua
     try:
         print("Enviando telemetría")
         url = "http://iot.ceisufro.cl:8080/api/plugins/telemetry/DEVICE/"+secrets["API"]["deviceId"]+"/SHARED_SCOPE"
@@ -114,8 +125,8 @@ def send_telemetry(): # Función para enviar señal a ThingsBoard cada vez que e
             'Accept': '*/*',
             'Accept-Encoding': 'gzip, deflate, br'
             }
-        data = '{"agua": 1}'
-        response = requests.post(url, data, headers=headers)
+        data = {key: 1}
+        response = requests.post(url, json=data, headers=headers)
         print(response.status_code)
     except Exception as e:
         print("Error de conexión:", e)
@@ -128,23 +139,46 @@ def getToken():
     response = requests.post(url, json=data, headers=headers)
     token = response.json()["token"]
 
+def detectCat():
+    # capture frames from a camera  
+    global cap
+    global isGatito
+    cap.start()
+    while True:  
+        # reads frames from a camera  
+        img = cap.capture_array() 
+        # convert to gray scale of each frames  
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  
+        # Detects faces of different sizes in the input image  
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5)  
+        if len(faces) > 1:  # Si len(faces) > 0 entonces se detectó un gato
+            print("Gatito Detectado")
+            isGatito = True
+        else:
+            print("Gatito no Detectado")
+            isGatito = False
+        time.sleep(1)
+
 if __name__ == '__main__':
     getToken()
+    # Definir hilos
     # Definir hilos
     t1 = threading.Thread(target=distanceMonitor, name='Distance Monitor', daemon=True)
     t2 = threading.Thread(target=subscriber, name='Subscriber', daemon=True)
     t3 = threading.Thread(target=modeSwitch, name='Mode Switch', daemon=True)
+    t4 = threading.Thread(target=detectCat, name='Cat Detection', daemon=True)
     # Iniciar hilos
     t1.start()
     t2.start()
     t3.start()
+    t4.start()
     try:
         # Esperar a que los hilos terminen
         t1.join()
         t2.join()
         t3.join()
+        t4.join()
     except KeyboardInterrupt:
         GPIO.cleanup()
-
 
 
