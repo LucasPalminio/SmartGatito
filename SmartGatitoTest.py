fca0e4c8f5ad7b600fdf3d200193e21b7836ac04
Felipe Sanzana <pipesanzana12@gmail.com>
2024-06-12 00:27:45 -0400

    Streaming

--- SmartGatitoTest.py
+++ SmartGatitoTest.py
@@ -5,6 +5,7 @@ import threading
 import json
 import requests
 import cv2 
+from queue import Queue
 from picamera2 import Picamera2
 from flask import Flask, Response
 
@@ -42,8 +43,11 @@ cap = Picamera2()
 isGatito = False
 detectado = False
 app = Flask(__name__)
-# Configuración MQTT
+# Global variable for shared image
+shared_image = None
+image_lock = threading.Lock()  # Ensure thread-safe access to shared_image
 
+# Configuración MQTT
 client = mqtt.Client(clientId)  # Crear nuevo objeto de instancia
 client.username_pw_set(username, password)  # Configurar usuario y contraseña
 client.on_connect = on_connect
@@ -141,66 +145,95 @@ def getToken():
     response = requests.post(url, json=data, headers=headers)
     token = response.json()["token"]
 
-def detectCat(img):
-    # capture frames from a camera  
-    global cap
+def generate_frames(output_queue):
     global isGatito
-    # reads frames from a camera  
-    img = cap.capture_array() 
-    # convert to gray scale of each frames  
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  
-    # Detects faces of different sizes in the input image  
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)  
-    if len(faces) > 1:  # Si len(faces) > 0 entonces se detectó un gato
-        print("Gatito Detectado")
-        isGatito = True
-    else:
-        print("Gatito no Detectado")
-        isGatito = False
-    time.sleep(1)
+    try:
+        last_time = time.time()
+        while True:
+            current_time = time.time()
+            with image_lock:   
+                img = shared_image  
+            if img is not None:
+                gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  
+                # Detects faces of different sizes in the input image  
+                faces = face_cascade.detectMultiScale(gray, 1.3, 5)  
+                # Draw rectangles around detected faces
+                for (x, y, w, h) in faces:
+                    cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+                # Calculate framerate
+                time_diff = current_time - last_time
+                if time_diff > 0:  # Prevent division by zero
+                    framerate = 1 / time_diff  # Calculate the framerate
+                    framerate_str = f"{framerate:.2f} FPS"
+                    cv2.putText(img, framerate_str, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+                # Encode the frame in JPEG format
+                ret, buffer = cv2.imencode('.jpg', img,[int(cv2.IMWRITE_JPEG_QUALITY), 55])
+                img = buffer.tobytes()
+                # Add the frame to the output queue
+                output_queue.put(img)
+                # Check if any cat faces were detected
+                if len(faces) > 0:
+                    print("Gatito Detectado")
+                    isGatito = True
+                else:
+                    print("Gatito no Detectado")
+                    isGatito = False
+                last_time = current_time
+    except Exception as e:
+        print(f"Error generating frames: {e}")
 
-def generate_frames():
-    global cap
-    global isGatito
-    preview_config = cap.create_preview_configuration(main={"size": (640, 480)})
-    cap.configure(preview_config)
-    cap.start()
-    #cap = cv2.VideoCapture(0)  # Use your camera setup
+def frame_generator(output_queue):
     while True:
-        frame = cap.capture_array()
-        detectCat(frame)
-        ret, buffer = cv2.imencode('.jpg', frame)
-        frame = buffer.tobytes()
+        frame = output_queue.get()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
+def cameraCapture():
+    global shared_image
+    while True:
+        # Capture image from camera
+        img = cap.capture_array()
+        with image_lock:
+            shared_image = img
+        time.sleep(0.01666)  # Adjust based on required capture rate
+
+
 @app.route('/video')
 def video():
-    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
-
-def run_app():
-    app.run(debug=False, host='0.0.0.0', threaded=True)
+    output_queue = Queue()   
+    # Definir hilos
+    print("Iniciando Stream")
+    t5 = threading.Thread(target=cameraCapture, name='Camera Frame capture', daemon=True)
+    t6 = threading.Thread(target=generate_frames,  args=(output_queue,), name='Video Streaming', daemon=True)
     
-if __name__ == '__main__':
+    # Iniciar hilos
+    t5.start()
+    t6.start()
+
+        
+
+    return Response(frame_generator(output_queue), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+def cameraStartup():
+    global cap
+    preview_config = cap.create_preview_configuration(main={"size": (320, 240)})
+    cap.configure(preview_config)
+    cap.start()
+
+def threadsInit():
+    print("Iniciando aplicación")
     getToken()
-    # Definir hilos
-    # Definir hilos
     t1 = threading.Thread(target=distanceMonitor, name='Distance Monitor', daemon=True)
     t2 = threading.Thread(target=subscriber, name='Subscriber', daemon=True)
     t3 = threading.Thread(target=modeSwitch, name='Mode Switch', daemon=True)
-    t4 = threading.Thread(target=run_app, name='Cat Detection', daemon=True)
-    # Iniciar hilos
+    t4 = threading.Thread(target=cameraStartup, name='Camera Startup', daemon=True)
     t1.start()
     t2.start()
     t3.start()
     t4.start()
-    try:
-        # Esperar a que los hilos terminen
-        t1.join()
-        t2.join()
-        t3.join()
-        t4.join()
-    except KeyboardInterrupt:
-        GPIO.cleanup()
+    print("Aplicación iniciada")
 
 
+if __name__ == '__main__':
+    threadsInit()
+    app.run(debug=False, host='0.0.0.0', threaded=True)
--- stream.py
+++ stream.py
@@ -1,26 +1,45 @@
 from flask import Flask, Response
 from picamera2.picamera2 import Picamera2
+from threading import Thread
 import cv2
 import numpy as np
+from queue import Queue
+import time
 
 app = Flask(__name__)
 
-def generate_frames():
+def generate_frames(output_queue):
     picam2 = Picamera2()
-    preview_config = picam2.create_preview_configuration(main={"size": (640, 480)})
+    preview_config = picam2.create_preview_configuration(main={"size": (320, 240)})
     picam2.configure(preview_config)
     picam2.start()
-
+    last_time = time.time()
     while True:
+        current_time = time.time()
         frame = picam2.capture_array()
-        ret, buffer = cv2.imencode('.jpg', frame)
+        time_diff = current_time - last_time
+        if time_diff > 0:  # Prevent division by zero
+            framerate = 1 / time_diff  # Calculate the framerate
+            framerate_str = f"{framerate:.2f} FPS"
+            cv2.putText(frame, framerate_str, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+        ret, buffer = cv2.imencode('.jpg', frame,[int(cv2.IMWRITE_JPEG_QUALITY), 85])
         frame = buffer.tobytes()
+        output_queue.put(frame)
+        last_time = current_time
+
+def frame_generator(output_queue):
+    while True:
+        frame = output_queue.get()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 @app.route('/video')
 def video():
-    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    output_queue = Queue()
+    t = Thread(target=generate_frames, args=(output_queue,))
+    t.start()
+    return Response(frame_generator(output_queue), mimetype='multipart/x-mixed-replace; boundary=frame')
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
